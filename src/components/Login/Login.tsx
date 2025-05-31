7b11471937a8b6323bdd9cbde6bad560c9d78c1e
Ujk768 <utkarsh.kanade1@gmail.com>
2025-05-31 19:29:57 +0530

    updated login compoennt

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,40 +1,36 @@
 import React from "react";
 import { useLogin } from "./useLogin";
-
+import "./login.scss";
 export const Login = () => {
   const { handleLogin, username, password, setPassword, setUsername } =
     useLogin();
   return (
-    <div>
-      <h1>Login Page</h1>
-      <p>Please enter your credentials to log in.</p>
-      <form>
-        <div>
-          <label htmlFor="username">Username: </label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit" onClick={handleLogin}>
-          Login
-        </button>
-      </form>
+    <div className="login-container">
+      <div className="login-container__section">
+        <div>Username : </div>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+      </div>
+      <div className="login-container__section">
+        <div>Password :</div>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit" onClick={handleLogin}>
+        Login
+      </button>
     </div>
   );
 };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Login } from "../../components/Login/Login";
-
+import "./home.scss";
 export const Home = () => {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
-        <Login/>
+      <h1>Welcome to 24K PG Dashboard</h1>
+      <Login />
     </div>
   );
 };
